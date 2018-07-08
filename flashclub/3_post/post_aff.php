b78329371246fb3f30ef707c9cbf08f4876055ec
AgnesTheApple <carletto.agnes@gmail.com>
2018-07-08 15:18:06 +0200

    first commit

---
+++ flashclub/3_post/post_aff.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>FlashClub-</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="icon" type="image/png" href="images/icon.png">
+		<link rel="stylesheet" href="css/forum.css">
+		<link rel="stylesheet" href="css/categories.css">
+		<link rel="stylesheet" href="css/aide.css"> 
+		<link rel="stylesheet" href="css/menu.css"> 
+    </head>
+	<body >
+	
+		<div class="col-12">
+			<div class="menu">
+			</div>
+			<div class="position" align="center">
+				<ul class="aide">
+					<li class="aide1"><A HREF="https://www.youtube.com/channel/UC60-h5I94Lj9DhYr6eZq0vg"><p>TUTORIEL</p></A></li>
+					<li class="aide2"><A HREF="https://www.capturenumerique.com/tutoriels/accessoires-photographes/"><p>OUTILS</p></A></li>
+				</ul>
+			</div>
+			<div class="position_menu" align="center">
+				<ul class="menuS">
+					<li class="menuS1" ><A HREF="../accueil_forum.html"><p>ACCUEIL</p></A></li>
+					<li class="menuS1" ><A HREF="../1-connexion_forum/connexion.php"><p>SE CONNECTER</p></A></li>
+					<li class="menuS1" ><A HREF="../1-inscription_forum/inscription.php"><p>S'INSCRIRE</p></A></li>
+				</ul>
+			</div>
+			<div class="position2" align="center">
+				<div class="diap2S">
+					<A HREF="https://twitter.com/"><img class="diap2" src="images/twi.png"/></A>
+					<A HREF="https://github.com/"><img class="diap2" src="images/git.png"/></A>
+					<A HREF="https://www.facebook.com/"><img class="diap2" src="images/fb2.png"/></A>
+				</div>
+			</div>
+		</div>
+		<p class="salut">Le forum des passionnés de photo</p>
+		<p class="titre" align="center">FlashClub-</p>
+		<div class="position1" align="center">
+			<img class="photo" src="images/photo.png"/>
+		</div>
+		<div class="position3" align="center">
+			<div class="position_categories">
+				<ul class="categories">
+					<li class="categories1"><A HREF=""><p>DISCUSSION</p></A></li>
+					<li class="categories1"><A HREF=""><p>DEBUTANT</p></A></li>
+					<li class="categories1"><A HREF=""><p>INTERMEDIAIRE</p></A></li>
+					<li class="categories1"><A HREF=""><p>CONFIRMER</p></A></li>
+				</ul>
+			</div>
+			<div class="discussion">
+				<table class="forum">
+					<tr class="header">
+		  		<!-- <th class="main center">Sujet></th> -->
+					</tr>
+					<tr>
+					<td><?= $auteur['pseudo'] ?></th>
+		  			<td class="sub-info w10"><?= $post['sujet'] ?></th>
+		  			<td><?= $post['contenu'] ?></td>
+					</tr>
+
+					<?php while($data1 = $auteur_reponses->fetch()) { ?>
+      				<td><?= $data1['pseudo'] ?></td>
+      				<?php } ?>
+			
+	  				<?php while($data = $reponses->fetch()) { ?>
+      				<td><?= $data['contenu'] ?></td>
+      				<?php } ?>	
+   					</tr>	
+   				</table>
+
+   				<form method="POST">
+      				<textarea placeholder="Votre réponse" name="reponse" style="width:80%"></textarea><br/>
+      				<input type="submit" name="submit" value="Poster ma réponse">
+      			</form>
+      			<p style="color:red;"><?php if(isset($erreur)) { echo $erreur; } ?></p>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ flashclub/3_post_connecte/post.php
@@ -0,0 +1,58 @@
+<?php
+	session_start();
+   	require('../php/config.php');
+	
+	//on check si le varible qui contient le titre du post defini ds accueil.aff.php exist
+		if(isset($_GET['titre'])) {
+
+			$titre = htmlspecialchars($_GET['titre']);
+			$id = htmlspecialchars($_GET['id']);
+			//on selectionne tous les champs avec lid selectionner par luser
+			$post = $bdd->prepare('SELECT * FROM f_topics WHERE id = ?');
+			//on place les champs dans un tablaeu
+		    $post->execute(array($id));
+		    $post = $post->fetch();
+
+		    $auteur = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+		    $auteur->execute(array($post['id_createur']));
+		    $auteur = $auteur->fetch();
+
+		    $reponses = $bdd->prepare('SELECT * FROM f_messages WHERE id_topic = ?');
+		    $reponses->execute(array($id));
+
+		    $reponse_auteur = $bdd->prepare('SELECT * FROM f_messages WHERE id_topic = ?');
+		    $reponse_auteur->execute(array($id));
+		    $reponse_auteur = $reponse_auteur->fetch();
+
+
+		    $auteur_reponses = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+		    $auteur_reponses->execute(array($reponse_auteur['id_posteur']));
+
+
+			if(isset($_POST['submit'],$_POST['reponse'])) {
+
+				$reponse = htmlspecialchars($_POST['reponse']);
+
+				if(isset($_SESSION['id'])) {
+					if(!empty($reponse)) {
+
+					$ins = $bdd->prepare('INSERT INTO f_messages(id_topic, id_posteur, contenu, date_heure_post) VALUES (?,?,?,NOW())');
+					$ins->execute(array($id, $_SESSION['id'], $reponse));
+
+					$erreur = "OK! Votre reponse a bien etait poste!";
+
+
+					}else {
+						$erreur = "Oups votre reponse est vide!!!!";
+					}
+				}else {
+						$erreur = "oups vous n'etes pas connecter...";
+				}
+			}
+			require('post_aff.php');
+		}else {
+			die('Erreur...');
+		}
+		
+?>
+		   	
